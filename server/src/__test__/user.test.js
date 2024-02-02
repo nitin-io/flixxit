2cec5b0fb8f467b2c90ea42f98d4949173c07638
Nitin Chaudhary <76252414+nitin-io@users.noreply.github.com>
2024-02-02 23:29:13 +0530

    Configure JEST for testing

---
+++ server/src/__test__/user.test.js
@@ -0,0 +1,11 @@
+import supertest from "supertest";
+
+describe("user", () => {
+  describe("login user", () => {
+    describe("given the correct username and password", () => {
+      it("should return 200 ok", () => {
+        expect(true).toBe(true);
+      });
+    });
+  });
+});
